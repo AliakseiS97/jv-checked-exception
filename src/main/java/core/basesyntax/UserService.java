ef7a388f34b6c74f26464e7f2bee11c8960e10c2
AliakseiS97 <aliakseis97@gmail.com>
2025-08-03 03:52:55 +0200

    Exception task.

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,17 @@ package core.basesyntax;
 
 public class UserService {
     public void registerUser(User user) {
-        //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            if (user.getPassword() != null && user.getRepeatPassword() != null) {
+                passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+                saveUser(user);
+            } else {
+                throw new PasswordValidationException("Password or Repeat password null");
+            }
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
